cc8ae9346834a95c3063c5ab07f57da871f52188
doyinarolan <77995247+doyinarolan@users.noreply.github.com>
2021-02-10 00:37:00 -0500

    Corrected spelling mistake

--- medium_assign.py
+++ medium_assign.py
@@ -1,41 +1,18 @@
-'''
-
-This file contains the functions that you need for completing this assignment. 
-
-1. expression_1() --> 30%
-2. expression_2() --> 40%
-3. expression_3() --> 30% 
-
-Failure to follow the variable name guides will lead to reduction of 10 points. 
-
-DO NOT EDIT THE FUNCTION NAMES.
-
-
-'''
+import math
 
 def expression_1(x):
-
-    '''
-    
-        Write a code that returns value for the following expression: x^3 - (2x + x^2) - 100 
-    
-    '''
-
-    pass
+    y = math.pow(x,3) - ((2*x) + math.pow(x,2)) -100
+    return y
 
 def expression_2(x, y):
-
-    '''
-        Write code that returns only the integer value of the following expression: (x^4 / 2y) - (3xy) + (6y / 7x)
-    '''
-
-    pass
+    z = (math.pow(x,4)/(2*y)) - (3*x*y) + ((6*y)/(7*x))
+    return int(z)
 
 
 def expression_3(x, y):
-    '''
-        Write code that returns the value of the following expression: (x^3 + y^2) / (x^2 - y^3)
-    '''
+    z = (math.pow(x,3) +math.pow(y,2)) / (math.pow(x,2) - math.pow(y,3))
+    return z
+    
 
 if __name__ == "__main__":
     
